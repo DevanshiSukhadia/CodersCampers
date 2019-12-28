97608e16d5ee83f73757d3619368be51448ef5f7
Devanshi Sukhadia <sukhadia.devanshi@gmail.com>
2019-12-28 17:43:51 +0530

    Update NumberGame.java

--- Assignment2/NumberGame.java
+++ Assignment2/NumberGame.java
@@ -5,62 +5,51 @@ public class NumberGame {
 
 	public static void main(String[] args) {
 		int number;
+		int tries = 5;
+		boolean win = false;
+
 		Scanner input = new Scanner(System.in);
-		
-		//Generate random number 
+
+		// Generate random number
 
 		Random r = new Random();
 		number = r.nextInt(100) + 1;
-		System.out.println(number);
+		// print random number
+		System.out.println("Random number is: " +number);
 
 		// ask user to pick a number
 		System.out.println("Pick a number between 1 and 100: ");
-		int inputNumber = input.nextInt();
 
-		if (inputNumber <= 100) {
-			if (inputNumber == number) {
-				System.out.println("you have picked correct number!");
-				
-			} 
-			else if (inputNumber > number) {
-				
-				for (int i = 0; i <= 5; i++) {
-					
-					System.out.println("pick a lower number: \n");
-					
-					int number1 = input.nextInt();
-					
-					if (number1 == number) {
-						System.out.println("you have picked correct number!");
-						break;
-					} else {
-						System.out.println("You lose, the number to guess was Number: " + number);
-						break;
-					} // END OF IF STATEMENT
-					
-				} // END OF FOR LOOP
-				
-			} // END OF WHILE LOOP 
-			else if (inputNumber < number) {
-				
-				for (int i = 0; i <= 5; i++) {
-					System.out.println("pick a Higher number: \n");
-				
-					int number1 = input.nextInt();
-					if (number1 == number) {
-						System.out.println("you have picked correct number!");
-						break;
-					} else {
-						System.out.println("You lose, the number to guess was Number: " + number);
-						break;
-					}
+		while (win == false && tries > 0) {
 
-				} // END OF FOR LOOP
-			} // END OF WHILE LOOP
-		} 
-		else {
-			System.out.println("Your guess is not between 1 and 100, please try again");
-		}
+			int guess = input.nextInt();
+			tries--;
+
+			if (tries == 0) {
+				System.out.println("Correct number is: " + number);
+				break;
+			} // END OF TRIES IF
+
+			if (guess <= 100) {
+
+				if (guess == number) {
+					win = true;
+					System.out.println("you have picked correct number!");
+
+				} else if (guess > number) {
+					System.out.println("Pick lower number: ");
+
+				} else if (guess < number) {
+					System.out.println("Pick higher number");
+
+				}
+
+			} // END OF GUESS IF
+			else {
+				System.out.println("Your guess is not between 1 and 100, please try again");
+
+			}
+		} // END OF WHILE LOOP
 
 	} // END OF MAIN METHOD
 }// END OF NUMBER CLASS
